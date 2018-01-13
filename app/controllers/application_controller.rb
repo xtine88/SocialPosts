19f2474aa89cfb5cd619332f1a99f6735a51802b
xtine88 <c.delafuente88@gmail.com>
2018-01-13 10:23:31 +0100

    added timezones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  around_action :user_time_zone, if: :current_user
+
+  private
+
+  def user_time_zone(&block)
+  	Time.use_zone(current_user.time_zone, &block)
+  end
+
 end
