32b21de25bf6c683a320897a6d696c08e390365d
xtine88 <c.delafuente88@gmail.com>
2018-01-13 11:49:42 +0100

    created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dashboard]
   def home
+  	if current_user
+  		redirect_to dashboard_path
+  	end
+  end
+
+  def dashboard
+  	
   end
 end
